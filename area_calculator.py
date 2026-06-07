3616eb5304699bea3c7f5978c6a29818dec4d585
osmannn22 <osmannn220396@gmail.com>
2026-06-08 02:57:27 +0600

    third task

---
+++ area_calculator.py
@@ -0,0 +1,11 @@
+rectangle_length = 5 #It is the length of rectangle
+rectangle_width = 10 #It is the width of rectangle
+
+"""
+To find out the area of the rectangle we have to use the following formula:
+The area of the rectangle is defined as: rectangle_length * rectangle_width 
+"""
+
+area_of_rectangle = rectangle_length * rectangle_width
+
+print(area_of_rectangle)
